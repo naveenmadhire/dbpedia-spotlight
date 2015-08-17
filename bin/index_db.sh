fe217d22af297f961a27c481391718c222413ba0
NAVEEN MADHIRE <vmadhire@indiana.edu>
2015-08-16 22:37:52 -0500

    Index_db Changes

--- bin/index_db.sh
+++ bin/index_db.sh
@@ -117,38 +117,55 @@ fi
 cd $BASE_DIR
 
 #Set up pig:
-if [ -d $BASE_WDIR/pig ]; then
-    echo "Updating PigNLProc..."
-    cd $BASE_WDIR/pig/pignlproc
+#if [ -d $BASE_WDIR/pig ]; then
+#    echo "Updating PigNLProc..."
+#    cd $BASE_WDIR/pig/pignlproc
+#    git reset --hard HEAD
+#    git pull
+#    mvn -T 1C -q assembly:assembly -Dmaven.test.skip=true
+#else
+#    echo "Setting up PigNLProc..."
+#    mkdir -p $BASE_WDIR/pig/
+#    cd $BASE_WDIR/pig/
+#    git clone --depth 1 https://github.com/dbpedia-spotlight/pignlproc.git
+#    cd pignlproc
+#    echo "Building PigNLProc..."
+#    mvn -T 1C -q assembly:assembly -Dmaven.test.skip=true
+#fi
+
+#Set up DBPedia WikiStats:
+if [ -d $BASE_WDIR/wikistats ]; then
+    cd $BASE_WDIR/wikistats/wikipedia-stats-extractor
     git reset --hard HEAD
     git pull
     mvn -T 1C -q assembly:assembly -Dmaven.test.skip=true
 else
-    echo "Setting up PigNLProc..."
-    mkdir -p $BASE_WDIR/pig/
-    cd $BASE_WDIR/pig/
-    git clone --depth 1 https://github.com/dbpedia-spotlight/pignlproc.git
-    cd pignlproc
-    echo "Building PigNLProc..."
+    echo "Setting up WikiStats Repo..."
+    mkdir -p $BASE_WDIR/wikistats/
+    cd $BASE_WDIR/wikistats/
+    git clone --depth 1 https://github.com/dbpedia-spotlight/wikipedia-stats-extractor
+    cd wikipedia-stats-extractor
+    echo "Building WikiStats Repo"
     mvn -T 1C -q assembly:assembly -Dmaven.test.skip=true
 fi
+
 
 # Stop processing if one step fails
 set -e
 
 if [ "$local_mode" == "true" ]; then
 
-  if [ ! -e "$BASE_WDIR/pig/pig-0.10.1/" ]; then
+  #if [ ! -e "$BASE_WDIR/pig/pig-0.10.1/" ]; then
     #Install pig:
-    cd $BASE_WDIR/pig
-    wget http://archive.apache.org/dist/pig/pig-0.10.1/pig-0.10.1-src.tar.gz
-    tar xvzf pig-0.10.1-src.tar.gz
-    rm pig-0.10.1-src.tar.gz
-    cd pig-0.10.1-src
-    ant jar
-  fi
+  #  cd $BASE_WDIR/pig
+  #  wget http://archive.apache.org/dist/pig/pig-0.10.1/pig-0.10.1-src.tar.gz
+  #  tar xvzf pig-0.10.1-src.tar.gz
+  #  rm pig-0.10.1-src.tar.gz
+  #  cd pig-0.10.1-src
+  #  ant jar
+  #fi
 
-  export PATH=$BASE_WDIR/pig/pig-0.10.1-src/bin:$PATH
+  #export PATH=$BASE_WDIR/pig/pig-0.10.1-src/bin:$PATH
 
   #Get the dump
   curl -# "http://dumps.wikimedia.org/${LANGUAGE}wiki/latest/${LANGUAGE}wiki-latest-pages-articles.xml.bz2" | bzcat > $WDIR/${LANGUAGE}wiki-latest-pages-articles.xml
@@ -206,55 +223,77 @@ fi
 
 #Adapt pig params:
 cd $BASE_DIR
-cd $1/pig/pignlproc
+#cd $1/pig/pignlproc
+cd $1/wikistats/wikipedia-stats-extractor
 
-PIGNLPROC_JAR="$BASE_WDIR/pig/pignlproc/target/pignlproc-0.1.0-SNAPSHOT.jar"
+#PIGNLPROC_JAR="$BASE_WDIR/pig/pignlproc/target/pignlproc-0.1.0-SNAPSHOT.jar"
+WIKISTATS_JAR="$BASE_WDIR/wikistats/wikipedia-stats-extractor/target/wikipedia-stats-extractor-1.0-SNAPSHOT-jar-with-dependencies.jar"
+
+#if [ "$local_mode" == "true" ]; then
+
+#  mkdir -p $WDIR/pig_out/$LANGUAGE
+
+#  PIG_INPUT="$WDIR/${LANGUAGE}wiki-latest-pages-articles.xml"
+#  PIG_STOPWORDS="$WDIR/stopwords.$LANGUAGE.list"
+#  TOKEN_OUTPUT="$WDIR/pig_out/$LANGUAGE/tokenCounts"
+#  PIG_TEMPORARY_SFS="$WDIR/pig_out/$LANGUAGE/sf_lookup"
+#  PIG_NE_OUTPUT="$WDIR/pig_out/$LANGUAGE/names_and_entities"
+
+#  PIG_LOCAL="-x local"
+
+#else
+
+#  PIG_INPUT="/user/$USER/${LANGUAGE}wiki-latest-pages-articles.xml"
+#  PIG_STOPWORDS="/user/$USER/stopwords.$LANGUAGE.list"
+#  TOKEN_OUTPUT="/user/$USER/$LANGUAGE/tokenCounts"
+#  PIG_TEMPORARY_SFS="/user/$USER/$LANGUAGE/sf_lookup"
+#  PIG_NE_OUTPUT="/user/$USER/$LANGUAGE/names_and_entities"
+
+  #PIG_LOCAL=""
+
+#fi
+
+#Run pig:
+#pig $PIG_LOCAL -param LANG="$LANGUAGE" \
+#    -param LOCALE="$2" \
+#    -param INPUT="$PIG_INPUT" \
+#    -param OUTPUT="$PIG_NE_OUTPUT" \
+#    -param TEMPORARY_SF_LOCATION="$PIG_TEMPORARY_SFS" \
+#    -param PIGNLPROC_JAR="$PIGNLPROC_JAR" \
+#    -param MACROS_DIR="$BASE_WDIR/pig/pignlproc/examples/macros/" \
+#    -m examples/indexing/names_and_entities.pig.params examples/indexing/names_and_entities.pig
+
+
+#pig $PIG_LOCAL -param LANG="$LANGUAGE" \
+#    -param ANALYZER_NAME="$4Analyzer" \
+#    -param INPUT="$PIG_INPUT" \
+#    -param OUTPUT_DIR="$TOKEN_OUTPUT" \
+#    -param STOPLIST_PATH="$PIG_STOPWORDS" \
+#    -param STOPLIST_NAME="stopwords.$LANGUAGE.list" \
+#    -param PIGNLPROC_JAR="$PIGNLPROC_JAR" \
+#    -param MACROS_DIR="$BASE_WDIR/pig/pignlproc/examples/macros/" \
+#    -m examples/indexing/token_counts.pig.params examples/indexing/token_counts.pig
 
 if [ "$local_mode" == "true" ]; then
 
-  mkdir -p $WDIR/pig_out/$LANGUAGE
+  INPUT="$WDIR/${LANGUAGE}wiki-latest-pages-articles.xml"
+  STOPWORDS="$WDIR/stopwords.$LANGUAGE.list"
 
-  PIG_INPUT="$WDIR/${LANGUAGE}wiki-latest-pages-articles.xml"
-  PIG_STOPWORDS="$WDIR/stopwords.$LANGUAGE.list"
-  TOKEN_OUTPUT="$WDIR/pig_out/$LANGUAGE/tokenCounts"
-  PIG_TEMPORARY_SFS="$WDIR/pig_out/$LANGUAGE/sf_lookup"
-  PIG_NE_OUTPUT="$WDIR/pig_out/$LANGUAGE/names_and_entities"
-
-  PIG_LOCAL="-x local"
+  curl -# "http://apache.mirrorcatalogs.com/spark/spark-1.4.1/spark-1.4.1-bin-hadoop2.6.tgz"  > $WDIR/
+  tar -xvf $WDIR/spark-1.4.1-bin-hadoop2.6.tgz
+  cd $WDIR/spark-1.4.1-bin-hadoop2.6/bin
+  ./spark-submit --class org.dbpedia.spotlight.wikistats.main --master local[5] --conf spark.sql.shuffle.partitions=6 $WIKISTATS_JAR $INPUT $STOPWORDS ${LANGUAGE} $WDIR/ EnglishStemmer
 
 else
 
-  PIG_INPUT="/user/$USER/${LANGUAGE}wiki-latest-pages-articles.xml"
-  PIG_STOPWORDS="/user/$USER/stopwords.$LANGUAGE.list"
-  TOKEN_OUTPUT="/user/$USER/$LANGUAGE/tokenCounts"
-  PIG_TEMPORARY_SFS="/user/$USER/$LANGUAGE/sf_lookup"
-  PIG_NE_OUTPUT="/user/$USER/$LANGUAGE/names_and_entities"
+  INPUT="/user/$USER/${LANGUAGE}wiki-latest-pages-articles.xml"
+  STOPWORDS="/user/$USER/stopwords.$LANGUAGE.list"
 
-  PIG_LOCAL=""
-
+  curl -# "http://apache.mirrorcatalogs.com/spark/spark-1.4.1/spark-1.4.1-bin-hadoop2.6.tgz"  > /user/$USER/
+  tar -xvf /user/$USER/spark-1.4.1-bin-hadoop2.6.tgz
+  cd /user/$USER/spark-1.4.1-bin-hadoop2.6/bin
+  ./spark-submit --class org.dbpedia.spotlight.wikistats.main --master "Master URL" --conf spark.sql.shuffle.partitions=6 $WIKISTATS_JAR $INPUT $STOPWORDS ${LANGUAGE} $WDIR/ EnglishStemmer
 fi
-
-#Run pig:
-pig $PIG_LOCAL -param LANG="$LANGUAGE" \
-    -param LOCALE="$2" \
-    -param INPUT="$PIG_INPUT" \
-    -param OUTPUT="$PIG_NE_OUTPUT" \
-    -param TEMPORARY_SF_LOCATION="$PIG_TEMPORARY_SFS" \
-    -param PIGNLPROC_JAR="$PIGNLPROC_JAR" \
-    -param MACROS_DIR="$BASE_WDIR/pig/pignlproc/examples/macros/" \
-    -m examples/indexing/names_and_entities.pig.params examples/indexing/names_and_entities.pig
-
-
-pig $PIG_LOCAL -param LANG="$LANGUAGE" \
-    -param ANALYZER_NAME="$4Analyzer" \
-    -param INPUT="$PIG_INPUT" \
-    -param OUTPUT_DIR="$TOKEN_OUTPUT" \
-    -param STOPLIST_PATH="$PIG_STOPWORDS" \
-    -param STOPLIST_NAME="stopwords.$LANGUAGE.list" \
-    -param PIGNLPROC_JAR="$PIGNLPROC_JAR" \
-    -param MACROS_DIR="$BASE_WDIR/pig/pignlproc/examples/macros/" \
-    -m examples/indexing/token_counts.pig.params examples/indexing/token_counts.pig
-
 
 #Copy results to local:
 cd $BASE_DIR
@@ -262,17 +301,17 @@ cd $WDIR
 
 if [ "$local_mode" == "true" ]; then
 
-  cat $TOKEN_OUTPUT/part* > tokenCounts
-  cat $PIG_NE_OUTPUT/pairCounts/part* > pairCounts
-  cat $PIG_NE_OUTPUT/uriCounts/part* > uriCounts
-  cat $PIG_NE_OUTPUT/sfAndTotalCounts/part* > sfAndTotalCounts
+  cat ./TokenCounts/part* > tokenCounts
+  cat ./PairCounts/part* > pairCounts
+  cat ./UriCounts/part* > uriCounts
+  cat ./TotalSfCounts/part* > sfAndTotalCounts
 
 else
 
-  hadoop fs -cat $LANGUAGE/tokenCounts/part* > tokenCounts
-  hadoop fs -cat $LANGUAGE/names_and_entities/pairCounts/part* > pairCounts
-  hadoop fs -cat $LANGUAGE/names_and_entities/uriCounts/part* > uriCounts
-  hadoop fs -cat $LANGUAGE/names_and_entities/sfAndTotalCounts/part* > sfAndTotalCounts
+  hadoop fs -cat ./TokenCounts/part* > tokenCounts
+  hadoop fs -cat ./PairCounts/part* > pairCounts
+  hadoop fs -cat ./UriCounts/part* > uriCounts
+  hadoop fs -cat ./TotalSfCounts/part* > sfAndTotalCounts
 
 fi
 
